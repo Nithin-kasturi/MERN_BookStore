80f1ed426fc01d467c16f8c72e47f077f7a50d09
Nithin-kasturi <107863285+Nithin-kasturi@users.noreply.github.com>
2024-02-04 23:04:04 +0530

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -12,7 +12,7 @@ export default function Register() {
     });
   const handleSubmit=async(e)=>{
     e.preventDefault();
-    axios.post('http://localhost:8000/addUser',userDetails).
+    axios.post('https://mern-book-store-livid.vercel.app/addUser',userDetails).
     then((res)=>{setResponse(res)});
     console.log(response);
   }
